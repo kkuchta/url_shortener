4ec5514b96809f64b28d69964c6117bd4d7514df
Kevin Kuchta <kevin@kevinkuchta.com>
2018-03-30 23:18:38 -0700

    Remvoe old js functions

--- runner.js
+++
@@ -1,7 +0,0 @@
-const func = require('./functions/hello/index.js');
-const { exec } = require('child_process');
-process.chdir('functions/hello');
-global.isLocal = true;
-func.handle(0, 0, (first, result) => {
-  console.log("result = ", result);
-});
