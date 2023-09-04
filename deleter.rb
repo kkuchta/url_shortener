cfa47fc91929f4dad31fa75c3eafed9a571a3cc4
Kevin Kuchta <kevin@kevinkuchta.com>
2023-09-04 12:45:56 -0700

    idk, forgot to commit; some puts lines?

--- deleter.rb
+++ deleter.rb
@@ -13,6 +13,7 @@ lambda = Aws::Lambda::Client.new(region: 'us-west-2', profile: 'apex')
 
 count_deleted = 0
 start = Time.now
+count_skipped = 0
 
 total = lambda.get_account_settings.account_usage.function_count
 puts "Total to delete: #{total}"
@@ -38,10 +39,15 @@ loop do
           count_deleted += 1
         else
           puts "skipping #{function.function_name}"
+          count_skipped += 1
         end
       end
       elapsed_time = Time.now - start
+      if count_skipped > 0
+        puts "Skipped #{count_skipped} lambdas"
+      end
       if count_deleted > 0
+        puts "Deleted #{count_deleted} lambdas"
         time_per_item = elapsed_time / count_deleted
         puts "Average seconds to delete an item: #{time_per_item}"
         time_left_seconds = (total - count_deleted) * time_per_item
